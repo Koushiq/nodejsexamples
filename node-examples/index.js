609763509979df4f321cf3720e3c604e643bb089
S.M Mushfiqur Rahman <koushiq1234@gmail.com>
2020-07-17 11:17:45 +0600

    initial commit

---
+++ node-examples/index.js
@@ -0,0 +1,22 @@
+var rect = require('./rectangle');
+
+
+function solveRect(l,b)
+{
+    console.log("Solving for rectangle with l = "+l+ " and b= "+b);
+
+    rect(l,b ,(err , rectangle) => {
+        if(err)
+        {
+            console.log("Error : "+err.message);
+        }
+        else
+        {
+            console.log("The area of the rectangle of dimensions l= "+l+" and b = "+b+" is "+rectangle.area());
+            console.log("The area of the perimeter of dimensions l= "+l+" and b = "+b+" is "+rectangle.perimeter());
+        }
+    });
+}
+
+solveRect(1,4);
+//console.log(rect.abc());
