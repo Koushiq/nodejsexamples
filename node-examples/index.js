d628a2eb1ace5a95de7144434353a4ce64d4d78c
S.M Mushfiqur Rahman <koushiq1234@gmail.com>
2020-07-24 21:11:55 +0600

    documentation added

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,50 +1,54 @@
 const http = require('http');
 const fs = require('fs');
 const path= require('path');
-const hostname = "localhost";
+const hostname = "127.0.0.5";
 const port = 3000;
 
 const server = http.createServer((req,res)=>   // req,res incoming http request to the servers !
 {
    // console.log(req.headers); //headers is a property of req object
     console.log("Request url : "+req.url+" request method : "+req.method);
-    if(req.method=="GET")
+    if(req.method=="GET") // check for request type
     {
-        let fileUrl;
-        if(req.url=="/")
+        let fileUrl; // variable declared to contain requested file's url
+        if(req.url=="/") //checking if user just requested the valid servername and port 
         {
-            fileUrl="/index.html";
-            
+            fileUrl="/index.html"; //if yes then url pointing to index.html
         }
         else
         {
-            fileUrl=req.url;
+            fileUrl=req.url; // else pointing to user's input url
         }
 
-        let filePath= path.resolve("./public"+fileUrl); // full path of the file
-        const fileExt = path.extname(filePath);  //extract file extension
-        console.log(filePath);
-        if(fileExt== ".html")
+        let filePath= path.resolve("./public"+fileUrl); // concating requested file path to absoulte path 
+        const fileExt = path.extname(filePath);  //extracting users requested file extension 
+        console.log(filePath); // printing full file path ( for debuggin purposes )
+        if(fileExt== ".html")  // checking if file extension is a html file or not
         {
-            fs.exists(filePath,(exist)=>{
-                if(!exist)
+            fs.exists(filePath,(exist)=>{ // calling a callback function using fs.exists to validate if file exists 
+                if(!exist) // if file doesn't exist showing correspoinding error code 
                 {
                     res.statusCode=404;
                     res.setHeader("Content-Type","text/html");
                     res.end(" <html>   <body>  <h1> Error "+res.statusCode+" file url at "+filePath+" not found ! </h1> </body> </html> ");
-                    return;
+                    return; // exiting the callback function
                 }
                 
                 res.statusCode=200;
                 res.setHeader("Content-Type","text/html");
-                fs.createReadStream(filePath).pipe(res); // read file from file path into bytestream and pipes the response 
+                //fs.createReadStream(filePath).pipe(res); // reads file from file path into bytestream and pipes the response ( createReadStream  reads the file stream , pipe writes the file stream to the response object)
+                fs.readFile(filePath, (err, data) => {
+                    if (err) throw err;
+                    //console.log(data); -> will display as buffered data to the console 
+                    res.write(data); // writes the response from the buffered data to html content , you can also use fs.createReadStream as alternative 
+                  });
             });
         }
         else
         {
             res.statusCode=404;
             res.setHeader("Content-Type","text/html");
-            res.end(" <html><body><h1> Error "+res.statusCode+" file url at "+filePath+" not an html file  ! </h1></body></html> ")
+            res.end(" <html><body><h1> Error "+res.statusCode+" file url at "+filePath+" not an html file  ! </h1></body></html> "); // setting appropiate error 
         }
 
     }
@@ -52,7 +56,7 @@ const server = http.createServer((req,res)=>   // req,res incoming http request 
     {
         res.statusCode=404;
         res.setHeader("Content-Type","text/html");
-        res.end(" <html><body><h1> Error "+res.method+" not supported </h1></body></html> ")
+        res.end(" <html><body><h1> Error "+res.method+" not supported </h1></body></html> "); // setting appropiate error 
     }
 
 });
