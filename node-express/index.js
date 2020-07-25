8919279f8d148a3204aed2eb26ae1c2d430f5cea
S.M Mushfiqur Rahman <koushiq1234@gmail.com>
2020-07-25 14:42:12 +0600

    morgan added

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 const hostname = "127.0.0.5";
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
+
 
 // next is a optional parameter
 app.use((req,res,next)=>{
-
-    console.log(req.headers);
+    
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1 style="font-weight:100;font-family:arial;">This is an Express Js Server </html></body></h1>');
